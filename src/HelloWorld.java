29fa53a2178456ea98fe89f4c72711780c75934c
zirochan <fuckoff123>
2018-03-13 23:24:08 +1300

    changed commit message

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,4 +4,6 @@ package PACKAGE_NAME;
  * Created by zihaoyang on 13/03/18.
  */
 public class HelloWorld {
+// Prints "Hello, World" to the terminal window.
+    System.out.println("Hello, World!");
 }
